1a7cf2b0c76088079f025f6c55e10da18b79f9a0
Faron-wsb <128227129+Faron-wsb@users.noreply.github.com>
2023-06-10 08:27:46 +0200

    Update Program.cs
    poprawa kodu

--- zadanie 3/zadanie 3/Program.cs
+++ zadanie 3/zadanie 3/Program.cs
@@ -1,40 +1,30 @@
-﻿using System;
+using System;
 
 public class MyProgram
 {
     public static void Main(string[] args)
     {
-        int a, b, c, d, x, y;
-
-        Console.WriteLine("podaj a:");
-        a = (int)inputValue();
-        Console.WriteLine("podaj b:");
-        b = (int)inputValue();
-        Console.WriteLine("podaj c:");
-        c = (int)inputValue();
-        Console.WriteLine("podaj d:");
-        d = (int)inputValue();
-        if (b == 0 || d == 0)
-        {
-            Console.WriteLine("Brak rozwiązania");
-        }
-        else
-        {
-            x = a * d + b * c;
-            y = b * d;
-            Console.Write("x=");
-            Console.WriteLine(x);
-            Console.Write("y=");
-            Console.WriteLine(y);
-        }
+        Console.WriteLine("podaj wagę w kg");
+        double w;
+        
+        w = inputValue();
+        Console.WriteLine("podaj wzrost w metrach");
+        double h;
+        
+        h = inputValue();
+        double bMI;
+        
+        bMI = w / (h * h);
+        Console.Write("BMI=");
+        Console.WriteLine(bMI);
     }
-
+    
     // .NET can only read single characters or entire lines from the
     // console. The following function safely reads a double value.
     private static double inputValue()
     {
         double result;
-        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        while (!double.TryParse(Console.ReadLine(), out result));
         return result;
     }
 }
